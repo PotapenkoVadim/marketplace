516fb98007b168c5f00a23538414ceaedba52664
PotapenkoVadim <potapenko.v00@mail.ru>
2022-12-15 17:09:28 +0300

    fix: fix home page introduce poster

--- src/components/pages/home/introduce/index.jsx
+++ src/components/pages/home/introduce/index.jsx
@@ -8,21 +8,19 @@ import poster from '../../../../static/introduce.png';
 
 export const HomePageIntroduce = () => {
   return (
-    <Container>
-      <div className={styles['introduce']}>
-        <div className={styles['introduce__info']}>
-          <div className={styles['introduce__subtitle']}>WE’RE STILL</div>
-          <h1 className={styles['introduce__title']}>Filling&nbsp;up the&nbsp;spaces.</h1>
-          <div className={styles['introduce__actions']}>
-            <Button variant='primary'>Explore Marketplace</Button>
-            <ButtonLink>See Top Deals</ButtonLink>
-          </div>
+    <div className={styles['introduce']}>
+      <Container className={styles['introduce__info']}>
+        <div className={styles['introduce__subtitle']}>WE’RE STILL</div>
+        <h1 className={styles['introduce__title']}>Filling up<br/>the spaces.</h1>
+        <div className={styles['introduce__actions']}>
+          <Button variant='primary'>Explore Marketplace</Button>
+          <ButtonLink>See Top Deals</ButtonLink>
         </div>
+      </Container>
 
-        <div className={styles['introduce__poster-container']}>
-          <Poster className={styles['introduce__poster']} source={poster} />
-        </div>
-      </div>
-    </Container>
+      <Poster
+        className={styles['introduce__poster']}
+        source={poster} />
+    </div>
   );
 };
